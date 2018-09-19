18d7362523cf41674191b7a46405379dc6eb0067
jatinsuneja <s.jatin1903@gmail.com>
2018-09-18 23:46:02 -0700

    setting up server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {console.log('you port is working')});
-
 app.get('/express-backend', (req, res)=> {
-    res.send({express: 'Your express backend is not connected to react'});
+    res.json({
+        ok: true
+    });
 });
 
+module.exports = app;
 // testing the backend with node js
