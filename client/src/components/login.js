3ac5410483883ff907f4bdd66cb6cf6de5c1f5b0
jatinsuneja <s.jatin1903@gmail.com>
2018-09-29 00:21:05 -0700

    basic auth boilerplate

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Login_Page extends Component {
     render () {
         return (
-            <h2>Login</h2>
+            <div>
+             <form>
+                 <div>
+            <TextField
+                label="email"
+                placeholder = "what's your email ?"
+                multiline
+            />
+            </div>
+            <div>
+            <TextField
+                label="password"
+                placeholder = "password here"
+                multiline
+            />
+            </div>
+
+
+                    <Button variant="contained" color="primary">
+                        Login
+                    </Button>
+
+
+                    <Button variant="outlined" color="secondary">
+                        Forgot password ? 
+                    </Button>
+            </form>
+            </div>
         )
     }
 }
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,9 +1,59 @@
 import React, {Component} from 'react';
 
+// import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+// import {withStyles} from '@material-ui/core/styles';
+
 class Register extends Component {
+
     render () {
         return (
-            <h2>Register</h2>
+            <div>
+                <form>
+                    <div>
+                    <TextField
+                        label="firstname"
+                        placeholder = "First Name"
+                        multiline
+                    />
+
+                    <TextField
+                        label="lastname"
+                        placeholder = "Last Name"
+                        multiline
+                    />
+                    </div>
+
+                    <div>
+                    <TextField
+                        label="email"
+                        placeholder = "enter your email here here"
+                        multiline
+                    />
+                    </div>
+
+                    <div>
+                    <TextField
+                        label="password"
+                        placeholder = "password here"
+                        multiline
+                    />
+                    </div>
+
+                    <div>
+                    <TextField
+                        label="confirm password "
+                        placeholder = "sssshhhh -2 "
+                        multiline
+                    />
+                    </div>
+
+                    <Button variant="contained" color="primary">
+                        Register
+                    </Button>
+                </form> 
+            </div>
         )
     }
 }
