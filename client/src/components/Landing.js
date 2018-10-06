8c3dd57e230fc0d64d3c6739f3938d2be377fb7e
jatinsuneja <s.jatin1903@gmail.com>
2018-10-06 01:25:20 -0700

    adding auth files to client for handling it

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
 
 class Landing extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            val: 'New'
+        }
+    }
+
+    onChange(e){
+        e.preventDefault();
+        this.setState({val: e.target.value})
+    }
+
     render () {
         return (
-            <h1>Landing</h1>
+            <div>
+                <h1>Landing</h1>
+                <hr/>
+                <form>
+                    <input type="text" value={this.state.val} onChange={this.onChange.bind(this)}/>
+                </form>
+                {this.state.val}
+            </div>
         )
     }
 }
